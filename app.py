8f492f7bbed234658ec2736ccd855e9a1e58abaf
Nalini Ajwani <98285552+nalini-ajwani@users.noreply.github.com>
2025-07-05 03:23:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ from flask_socketio import SocketIO, emit
 from datetime import datetime
 from flask_cors import CORS
 
-app = Flask(__name__, static_folder='static', template_folder='.')
+app = Flask(__name__, static_folder='static', template_folder='templates')
 CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-ladle_data = []  # Store history temporarily
+ladle_data = []
 
 @app.route('/')
 def index():
-    return render_template('ladle_monitoring_ui.html')  # Your HTML file
+    return render_template('ladle_monitoring_ui.html')
 
 @socketio.on('new_entry')
 def handle_new_entry(data):
